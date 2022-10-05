2da180e613bcd5bc1ab55e8d9be73fa033e57af1
Ujwal Gulhane <72155234+Ujwalgulhane@users.noreply.github.com>
2022-10-05 21:50:03 +0530

    Now, It is more optimal Solutions
    Time complexity -> O(n)
    Space complexity -> O(1)

--- C++/primeornot.cpp
+++ C++/primeornot.cpp
@@ -1,26 +1,35 @@
 //Hacktoberfest 2022
 /* 
-Time complexity -> O(n * log (log n))
-Space complexity -> O(n)
+Time complexity -> O(n)
+Space complexity -> O(1)
 */
-class Solution {
-public:
-    int countPrimes(int n) {
-        /* intialize vector all numbers are intially prime
-        traverse number from 2 to n - 1 if prime then count++
-        */
-        vector<bool> prime(n+1, true);
-        int count = 0;
-        prime[0] = prime[1] = false;
-        for(int i = 2; i < n; i++){
-            if(prime[i]){
-                count++;
-            }
-            for(int j = i*2; j < n; j = j + i){
-                prime[j] = false;
-            }
-        }
-        return count;
+
+#include <iostream>
+using namespace std;
+bool isPrime(int n){
+    if(n==0 || n==1){
+        return false;
     }
-};
+    for(int i=2; i < n; i++){
+        if(n % i == 0){
+            return false;
+        }
+    }
+    return true;
+}
+int main()
+{
+    int n;
+    cout << "Enter a Number -> ";
+    cin >> n;
+    if(isPrime(n)){
+        cout << "It is Prime Number...." << endl;
+    }
+    else{
+        cout << "It is not Prime Number...." << endl;
+    }
+    return 0;
+}
+
+#include
 //Contributed by Ujjwal Gulhane.
