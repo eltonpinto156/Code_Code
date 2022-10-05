939d915ff6e82ab8b020971bef6efd1f0112d91c
Ujwal Gulhane <72155234+Ujwalgulhane@users.noreply.github.com>
2022-10-05 21:40:42 +0530

    now, It is more optimal
    Time complexity -> O(n * log (log n))
    Space complexity -> O(n)

--- C++/primeornot.cpp
+++ C++/primeornot.cpp
@@ -1,24 +1,26 @@
-// 14) Program to check whether number is prime or not
-#include<bits/stdc++.h>
-using namespace std;
-
-int main(){
-    int num;
-    cin>>num;
-
-    int flag = 1;
-    for(int i = 2; i <= num/2; i++){
-        if(num % i == 0){
-            flag = 0;
-            break;
+//Hacktoberfest 2022
+/* 
+Time complexity -> O(n * log (log n))
+Space complexity -> O(n)
+*/
+class Solution {
+public:
+    int countPrimes(int n) {
+        /* intialize vector all numbers are intially prime
+        traverse number from 2 to n - 1 if prime then count++
+        */
+        vector<bool> prime(n+1, true);
+        int count = 0;
+        prime[0] = prime[1] = false;
+        for(int i = 2; i < n; i++){
+            if(prime[i]){
+                count++;
+            }
+            for(int j = i*2; j < n; j = j + i){
+                prime[j] = false;
+            }
         }
+        return count;
     }
-
-    if(flag == 1){
-        cout<<num<<" is Prime."<<endl;
-    }
-    else{
-        cout<<num<<" is not Prime."<<endl;
-    }
-    return 0;
-}
+};
+//Contributed by Ujjwal Gulhane.
